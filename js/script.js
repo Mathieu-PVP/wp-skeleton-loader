19eeae4651eb37bac5cc1d8b128c46f40792fb14
Mathieu-PVP <mathieu.gallaisdechateaucroc@pvp.fr>
2024-05-16 12:00:57 +0200

    add comments

--- js/script.js
+++ js/script.js
@@ -7,14 +7,17 @@
  */
 
 
+// Si jQuery n'est pas chargé, une erreur est retournée
 if (typeof jQuery === 'undefined') {
     throw new Error('jQuery n\'a pas été chargé !');
 }
 
+// Si $ n'est pas déclarée dans un script plus haut, alors on l'a déclare
 if (typeof $ === 'undefined') {
     var $ = jQuery;
 }
 
+// Dès que le DOM est chargé, on recouvre les images
 $(document).ready(function() {
     $('.skeleton-loader img').each(function() {
         const overlay = $('<div>').addClass('skeleton-overlay');
@@ -23,6 +26,7 @@ $(document).ready(function() {
     });
 });
 
+// Dès que la page est entièrement chargée, on enlève l'effet skeleton et découvre les images après un délai de 2s 
 $(window).on('load', function() {
     setTimeout(() => {
         $('.skeleton-overlay').each(function() {
